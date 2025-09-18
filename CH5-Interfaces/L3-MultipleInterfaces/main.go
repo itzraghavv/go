2eee9d1e2973020ad4a42ee8bb4a0c892e2d460f
raghav-97 <raghavtandulkar97@gmail.com>
2025-09-19 01:23:47 +0530

    ch5-interfaces

---
+++ CH5-Interfaces/L3-MultipleInterfaces/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func (e email) cost() int {
+	if !e.isSubscribed {
+		return 5 * len(e.body)
+	}
+	return 2 * len(e.body)
+}
+
+func (e email) format() string {
+	// ?
+	if !e.isSubscribed {
+		return fmt.Sprintf("'%s' | Not Subscribed", e.body)
+	}
+	return fmt.Sprintf("'%s' | Subscribed", e.body)
+}
+
+type expense interface {
+	cost() int
+}
+
+type formatter interface {
+	format() string
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+}
